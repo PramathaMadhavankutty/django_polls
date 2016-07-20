b492134f637d429a4b656d6118d6e04f3a724d4c
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-20 07:46:21 -0400

    modify

--- threads/forms.py
+++ threads/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Thread, Posts
 
 class ThreadForm(forms.ModelForm):
+
+    name = forms.CharField(label='Thread name')
+    is_a_poll = forms.BooleanField(label='Include a poll?', required=False)
     class Meta:
         model = Thread
         fields = ['name']
