2efb3db68420ca1741fcc20e8cd27566e1f4d8a4
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-20 07:25:05 -0400

    add polls app

--- We_Are_Social/settings.py
+++ We_Are_Social/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'products',
     'magazines',
     'threads',
+    'polls',
 
 
 ]
---
+++ polls/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.conf import settings
+from threads.models import Thread
+
+# Create your models here.
+class Poll(models.Model):
+
